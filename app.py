ecb3bf08c54e8d660fc434148c373a4671337cd8
Lich-senpai13 <papesenki.enrico@iisgalvanimi.edu.it>
2022-02-22 12:29:54 +0000

    sitoN°0.base

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return render_template("Home.html") #importa il file html
+
+@app.route('/it', methods=['GET'])
+def ciao_Mondo():
+    return render_template("Home.html") #importa il file html
+
+
+@app.route('/fr', methods=['GET'])
+def bonjure():
+    return ('<h1>bonjure, monde!</h1>')
+
+
+
+# da qui non si puo mettere nientaltro 
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
+
