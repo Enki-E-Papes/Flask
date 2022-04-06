961a368980cac99662a531353098daf92b846a7d
Lich-senpai13 <papesenki.enrico@iisgalvanimi.edu.it>
2022-04-06 12:54:56 +0000

    FIO

--- esGebrus.py
+++ esGebrus.py
@@ -1,19 +1,18 @@
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, send_file, make_response, url_for, Response, request, redirect
 app = Flask(__name__)
-
-
-import matplotlib.pyplot as plt
-import matplotlib
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-import contextily
-import geopandas as gpd
-import io
 import pandas as pd
+import io
+import geopandas as gpd
+import contextily
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 stazioni = pd.read_csv("/workspace/Flask/templates/ingressi_areac_varchi.csv",sep= ";")
 stazionigeo=gpd.read_file("/workspace/Flask/templates/ds710_coordfix_ripetitori_radiofonici_milano_160120_loc_final.geojson",sep= ";") 
-quartieri =gpd.read_file("/workspace/Flask/templates/GeoDataFrameLichSenpai13^ds964_nil_wm.zip")
+quartieri =gpd.read_file("/workspace/Flask/templates/Quartieri.zip")
 
 @app.route('/', methods=['GET'])
 def home():
@@ -45,8 +44,12 @@ def grafico():
 def selezione():
     scelta=request.args["scelta"]
     if scelta == "es1":
-        return redirect(url_for("/numero"))
-    return render_template("home.html")
+        return redirect(url_for('numero'))
+    elif scelta == "es2":
+        return redirect(url_for('input'))
+    else:
+        scelta == "es3"
+        return redirect(url_for('drop_down'))
 
 @app.route('/input', methods=['GET'])
 def input():
@@ -56,15 +59,21 @@ def input():
 @app.route('/ricerca', methods=['GET'])
 def ricerca():
     global quartiere,stazioni_quartire
-    nomequartiere=request.args["quartiere"]
-    quartiere=quartieri[quartieri.NIL.str.contains(nomequartiere)]
-    stazioni_quartire= stazionigeo[stazionigeo.within(quartiere.unary_union)]
-    return render_template("elenco1.html",risultato=stazioni_quartire.to_html())
+    nomequartiere=request.args["ricerca"]
+    quartiere=quartieri[quartieri['NIL'].str.contains(nomequartiere)]
+    stazioni_quartire=  stazionigeo[stazionigeo.within(quartiere.unary_union)]
+    return render_template("elenco1.html",risultato=stazionigeo.to_html())
+    #risultato = Stazioni_Radio.to_html()
 
 @app.route('/mappa', methods=['GET'])
 def mappa():
-    fig,ax =plt 
-   
+    fig, ax = plt.subplots(figsize = (12,8))
+    stazioni_quartire.to_crs(epsg=3857).plot(ax=ax, alpha=0.5, facecolor='k')
+    quartiere.to_crs(epsg=3857).plot(ax=ax, alpha=0.5, facecolor='k')
+    contextily.add_basemap(ax=ax)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
     return render_template("elenco.html",risultato = risultato.to_html())
 
 @app.route('/drop_down', methods=['GET'])
