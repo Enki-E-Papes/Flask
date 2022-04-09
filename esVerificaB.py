94dde1f3a70aeb996a3f0f851a9cc30f087382d9
Lich-senpai13 <papesenki.enrico@iisgalvanimi.edu.it>
2022-04-09 14:58:28 +0000

    こんにちは

---
+++ esVerificaB.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, send_file, make_response, url_for, Response, request, redirect
+app = Flask(__name__)
+import pandas as pd
+import io
+import geopandas as gpd
+import contextily
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+Stazioni = pd.read_csv("/workspace/Flask/templates/ingressi_areac_varchi.csv",sep= ";")
+Stazioni_Geo=gpd.read_file("/workspace/Flask/templates/ds710_coordfix_ripetitori_radiofonici_milano_160120_loc_final.geojson",sep= ";") 
+Quartieri=gpd.read_file("/workspace/Flask/templates/Quartieri.zip")
+risultato = Stazioni.groupby('MUNICIPIO')['OPERATORE'].count().reset_index()
+
+@app.route('/', methods=['GET'])
+def homepage():
+    return render_template("home1B.html")
+
+@app.route('/radio', methods=['GET'])
+def radio():
+    Quartieri1 = Quartieri['NIL'].drop_duplicates().to_list()
+    Quartieri1.sort()
+    return render_template("radioVerificaB.html", quartieri = Quartieri1)
+
+@app.route('/elenco', methods=['GET'])
+def elenco():
+    Quartiere = request.args['radio']
+    Quartiere_Trovato = Quartieri[Quartieri['NIL'] == Quartiere]
+    Stazioni_Radio = Stazioni_Geo[Stazioni_Geo.within(Quartiere_Trovato.unary_union)]
+
+    return render_template('elencoVerificaB.html',Radio_Trovati = Stazioni_Radio['OPERATORE'].to_list())
+
+@app.route('/input', methods=['GET'])
+def input():
+    return render_template("inputVerificaB.html")
+
+@app.route('/ricerca', methods=['GET'])
+def ricerca():
+    Quartiere_Cercato = request.args['ricerca']
+    Quartiere_Trovato = Quartieri[Quartieri['NIL'].str.contains(Quartiere_Cercato)].to_crs(epsg=3857)
+    Stazioni_Radio = Stazioni_Geo[Stazioni_Geo.to_crs(epsg=3857).within(Quartiere_Trovato.unary_union)]
+    fig, ax = plt.subplots(figsize = (12,8))
+
+    Stazioni_Radio.to_crs(epsg=3857).plot(ax=ax, facecolor='k', alpha=0.5)
+    contextily.add_basemap(ax=ax)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/numero', methods=['GET'])
+def numero():
+    return render_template("numeroVerificaB.html",risultato = risultato.to_html())
+
+@app.route('/grafico', methods=['GET'])
+def grafico():
+    fig, ax = plt.subplots(figsize = (6,4))
+
+    x = risultato.MUNICIPIO
+    y = risultato.OPERATORE
+
+    ax.bar(x, y, color = "#304C89")
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
---
+++ esVerificaC.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, send_file, make_response, url_for, Response, request, redirect
+app = Flask(__name__)
+import pandas as pd
+import io
+import geopandas as gpd
+import contextily
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+Bus_Tram = gpd.read_file('/workspace/Flask/ds964_nil_wm.zip')
+Quartieri = gpd.read_file('/workspace/Flask/templates/Quartieri.zip')
+
+
+@app.route('/', methods=['GET'])
+def homepage():
+    return render_template("home1C.html")
+
+@app.route('/selezione', methods=['GET'])
+def selezione():
+    scelta = request.args['scelta']
+    if scelta == 'Esercizio1':
+        return redirect(url_for('input'))
+    elif scelta == 'Esercizio2':
+        return redirect(url_for('input1'))
+    else:
+        return redirect(url_for('scelta'))
+
+
+@app.route('/input', methods=['GET'])
+def input():
+    return render_template("inputVerficaC.html")
+
+@app.route('/elenco', methods=['GET'])
+def elenco():
+    L_Minima = request.args['elenco1']
+    L_Massima = request.args['elenco2']
+    Bus_Tram1 = Bus_Tram[(Bus_Tram['lung_km'] < L_Massima) & (Bus_Tram['lung_km'] > L_Minima)]
+    Bus_Tram1 = Bus_Tram1['linea'].drop_duplicates().to_list()
+    Bus_Tram1.sort(key=int)
+    return render_template("Correzione_Verifica_C/Elenco.html",Bus_Tram = Bus_Tram1)
+
+@app.route('/input1', methods=['GET'])
+def input1():
+    return render_template("Correzione_Verifica_C/Input1.html")
+
+@app.route('/elenco1', methods=['GET'])
+def elenco1():
+    Quartiere_Cercato = request.args['quartiere']
+    Quartiere_Trovato = Quartieri[Quartieri['NIL'].str.contains(Quartiere_Cercato)]
+    Bus_Tram1 = Bus_Tram[Bus_Tram.intersects(Quartiere_Trovato.unary_union)]
+    Bus_Tram1 = Bus_Tram1['linea'].drop_duplicates().to_list()
+    Bus_Tram1.sort(key=int)
+    return render_template("Correzione_Verifica_C/Elenco.html",Bus_Tram = Bus_Tram1)
+
+@app.route('/scelta', methods=['GET'])
+def scelta():
+    Bus_Tram1 = Bus_Tram['linea'].drop_duplicates().to_list()
+    Bus_Tram1.sort(key=int)
+    return render_template("Correzione_Verifica_C/Scelta.html",linea = Bus_Tram1)
+
+@app.route('/linea', methods=['GET'])
+def Plot():
+        Linea_Scelto = request.args['linee']
+        Plot = Bus_Tram[Bus_Tram['linea']==Linea_Scelto]
+        fig, ax = plt.subplots(figsize = (12,8))
+
+        Plot.to_crs(epsg=3857).plot(ax=ax, alpha=0.5)
+        contextily.add_basemap(ax=ax)
+        output = io.BytesIO()
+        FigureCanvas(fig).print_png(output)
+        return Response(output.getvalue(), mimetype='image/png')
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
